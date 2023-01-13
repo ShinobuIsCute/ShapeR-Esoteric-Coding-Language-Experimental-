1fd2445afb1780cbabc8e0ef15b38beaa5b276e3
blazalage <karlethancamus@gmail>
2023-01-14 09:42:47 +1300

    Idk how we fixed ShapeFinder

--- Compiler.cpp
+++ Compiler.cpp
@@ -7,7 +7,8 @@
 
 using namespace std; // Just makes it so you don't need std::
 
-int* shape_finder(string* text);
+int resizeOrder(int* iref, int* jref, int* shapeOrder, int orderSize, int shapeID, bool skipLine);
+void shape_finder(string* text, int* shapeOrder, int num_lines);
 int count_char_in_line(string line);
 int check_rhombus(string* text, int start);
 
@@ -16,12 +17,12 @@ int main() {
         int num_char_in_file = 0;
         int numRows; // Amount of rows in ShapeArray
         int numCols;
+        //int *orderSize = 0;
         string fileName; //Take filename from terminal, can make it so it's just a set string if needed
         string tempLine; //temporarily stores lines of the txt file
         string redacted; //Just used to make the line counter work
         cout << "Enter Filename: Test.txt\n";
         fileName = "Test.txt";
-        //cin >> fileName;
         
         ifstream originalFile; // I think ifstream is for reading, and ofstream is for writing
         originalFile.open(fileName); // Original File is the input
@@ -46,7 +47,6 @@ int main() {
         counter = 0; //Re-using counter as an index for ShapeArray here
         while(!originalFile.eof()) { //Store the line
                 getline(originalFile, tempLine, '\n');
-                cout << tempLine << "\n";
                 new (ShapeArray + counter) string(tempLine);
                 counter++;
         }
@@ -55,20 +55,31 @@ int main() {
                 cout << ShapeArray[row];
                 printf("\n");
         }
+        
         printf("Hello there!\n");
-        //cin >> redacted;// Pause to see output
 
         counter = -1; //Now, counter will be used for the compiler itself.
 
         originalFile.close();
         //printf("Before ShapeOrder");
-        int* ShapeOrder = shape_finder(ShapeArray); //Placeholder ShapeOrder array here
+        int ShapeOrder[100];
+        for(int i = 0; i < 100; i++) {
+                ShapeOrder[i]= 0;
+                //printf("%d\n",ShapeOrder[i]);
+        }
+        printf("gay");
+        shape_finder(ShapeArray, ShapeOrder, /*orderSize,*/ numRows); //Placeholder ShapeOrder array here
+
+        cout << "Order Size: " << /**orderSize <<*/ "\n";
+
+
+        //for(int i = 0; i < )
 
         //Manager function here
-        for(int index = 0; index < sizeof(ShapeOrder) / sizeof(int); index++) {
-                cout << ShapeOrder[index];
+        for(int index = 0; index < 100; index++) {
+                cout << ShapeOrder[index] << "\n";
                 switch(ShapeOrder[index]) {
-                        case 0: //Square / rectangle
+                        case 9: //Square / rectangle
                                 break;
                         case 1: //up 
                                 break;
@@ -98,8 +109,6 @@ int main() {
         }
         
         printf("HELLO");
-        free(ShapeArray);
-        free(ShapeOrder);
         compiled.close();
         // tested cin >> numCols;
         return 0;
@@ -107,7 +116,7 @@ int main() {
 
 int count_char_in_line(string line) {
         int char_counter = line.length();
-        for (int i = 0; i < line.length(); ++i) {
+        for (int i = 0; i < line.length(); i++) {
                 if ((line[i] == ' ') || line[i] == '\0') {
                         char_counter = char_counter - 1;
                 }
@@ -116,135 +125,137 @@ int count_char_in_line(string line) {
 }
 
 int check_rhombus(string* text, int start) {
-        int i, j = 0;
-        int checker = 1;
-        if (text[0][0] == ' ') {
+        int i = 0;
+        int j = 0;
+        
+        if (text[start][0] != ' ') {
                 return 0;
         }
         while (count_char_in_line(text[start+j]) != 0) {
                 ++j;
         }
         --j;
-        while (i != j) {
-                if (sizeof(text[start+i]) == sizeof(text[start+j])) {
-                        continue;
+
+        int x = j/2;
+        
+        int m = count_char_in_line(text[start+i+1]);
+        int n = count_char_in_line(text[start+i]);
+        int test = m - n;
+        i++;
+
+        if (i == x) {
+                if (((text[start] == " ~ ") && (text[start+2] == " ~ ")) && (text[start+1] == "~~~")) {
+                        return 2;
                 } else {
-                        checker = 0;
-                        break;
+                        return 0;
                 }
         }
-        return checker;
+
+        for(i; i < x; i++) {
+                m = count_char_in_line(text[start+i+1]);
+                n = count_char_in_line(text[start+i]);
+                int checker = m - n;
+                if (test != checker) {
+                        return 0;
+                }
+        }
+
+        for(i; i < j; i++) {
+                m = count_char_in_line(text[start+i+1]);
+                n = count_char_in_line(text[start+i]);
+                int checker = m - n;
+                if (test != (checker - 2 * checker)) {
+                        return 0;
+                }
+        }
+
+        return 1;
 }
 
-int* shape_finder(string* text) {
-
+void shape_finder(string* text, int* shapeOrder, /*int *orderSize,*/ int num_lines) {
         int i = 0;
-        int array_size = 0;
-        int* array_to_return = (int*) malloc(sizeof(int));
+        int j = 0;
+        int* iref = &i;
+        int* jref = &j;
+        int ij [2];
         int* tmp;
+        int array_index = 0;
+        cout << "Initializing, NumLines : " << num_lines << "\n";
 
-        while (text[i] != "\u001a") {
+        while (i < num_lines) {
                 
-                if (i == 0) {
-                        if (count_char_in_line(text[i]) == 0) {
-                        ++i;
-                        }
-                } else {
-                        while (count_char_in_line(text[i]) != 0) {
-                                ++i;
-                        } //fix??
+                while ((count_char_in_line(text[i])) == 0 && (i < num_lines - 1)) {
+                        i++;
+                        cout << i << "\n";
+                } 
+                
+                j = i;
+                while (count_char_in_line(text[j]) != 0 && i < num_lines - 1) {
+                        ++j;
                 }
+                --j;
+                cout << "J : " << j;
                  
                 if (count_char_in_line(text[i]) == 1) {
                         if (check_rhombus(text, i) == 1) {
                                 //place 7 in array and then continue the loop
-                                ++array_size;
-                                tmp = (int*) realloc((int*) array_to_return, sizeof(int)*(array_size+1));
-                                if (tmp == NULL) {
-                                        perror("realloc failed");
-                                }
-                                array_to_return[array_size] = 7;
+                                array_index = resizeOrder(iref, jref, shapeOrder, array_index, 7, true);
                                 continue;
-                        }
-                
-                        int j = i;
-                        while (count_char_in_line(text[j]) != 0) {
-                                ++j;
-                        }
-                        --j;
-                        if (count_char_in_line(text[j]) != 1) {
+                        } else if (check_rhombus(text, i) == 2) {
+                                // place 8 in array and then continue the loop
+                                array_index = resizeOrder(iref, jref, shapeOrder, array_index, 8, true);
+                                continue;
+                        } else if (count_char_in_line(text[j]) != 1) {
                                 // place 1 in array and then continue the loop
-                                ++array_size;
-                                tmp = (int*) realloc((int*) array_to_return, sizeof(int)*(array_size+1));
-                                if (tmp == NULL) {
-                                        perror("realloc failed");
-                                }
-                                array_to_return[array_size] = 1;
+                                array_index = resizeOrder(iref, jref, shapeOrder, array_index, 1, true);
                                 continue;
-                        }
-                        if (text[j][0] == ' ') {
+                        } else if (text[j][0] == ' ') {
                                 // place 4 in array and then continue loop
-                                ++array_size;
-                                tmp = (int*) realloc((int*) array_to_return, sizeof(int)*(array_size+1));
-                                if (tmp == NULL) {
-                                        perror("realloc failed");
-                                }
-                                array_to_return[array_size] = 4;
+                                array_index = resizeOrder(iref, jref, shapeOrder, array_index, 4, true);
                                 continue;
                         } else {
                                 // place 2 in array and then continue loop 
-                                ++array_size;
-                                tmp = (int*) realloc((int*) array_to_return, sizeof(int)*(array_size+1));
-                                if (tmp == NULL) {
-                                        perror("realloc failed");
-                                }
-                                array_to_return[array_size] = 2;
+                                array_index = resizeOrder(iref, jref, shapeOrder, array_index, 2, true);
                                 continue;
                         }
                 } else {
-                        int j = i;
-                        while (count_char_in_line(text[j]) != 0) {
-                                ++j;
-                        }
-                        --j;
                         if (count_char_in_line(text[j]) == 1) {
                                 // place 3 in array and then continnue loop
-                                ++array_size;
-                                tmp = (int*) realloc((int*) array_to_return, sizeof(int)*(array_size+1));
-                                if (tmp == NULL) {
-                                        perror("realloc failed");
-                                }
-                                array_to_return[array_size] = 3;
+                                array_index = resizeOrder(iref, jref, shapeOrder, array_index, 3, true);
                                 continue;
                         } else if (count_char_in_line(text[i]) == count_char_in_line(text[j])) {
                                 // place 0 in array and then continue loop
-                                ++array_size;
-                                tmp = (int*) realloc((int*) array_to_return, sizeof(int)*(array_size+1));
-                                if (tmp == NULL) {
-                                        perror("realloc failed");
-                                }
-                                array_to_return[array_size] = 0;
+                                //cout << "J : " << j << "\n";
+                                array_index = resizeOrder(iref, jref, shapeOrder, array_index, 9, false);
+                                
                                 continue;
                         } else if (count_char_in_line(text[i]) < count_char_in_line(text[j])) {
                                 // place 5 in array and then continue loop
-                                ++array_size;
-                                tmp = (int*) realloc((int*) array_to_return, sizeof(int)*(array_size+1));
-                                if (tmp == NULL) {
-                                        perror("realloc failed");
-                                }
-                                array_to_return[array_size] = 5;
+                                array_index = resizeOrder(iref, jref, shapeOrder, array_index, 5, true);
                                 continue;
                         } else if (count_char_in_line(text[i]) > count_char_in_line(text[j])) {
                                 // place 6 in array and then continue loop
-                                ++array_size;
-                                tmp = (int*) realloc((int*) array_to_return, sizeof(int)*(array_size+1));
-                                if (tmp == NULL) {
-                                        perror("realloc failed");
-                                }
-                                array_to_return[array_size] = 6;
+                                array_index = resizeOrder(iref, jref, shapeOrder, array_index, 6, true);
                                 continue;
                         }
                 }
         }
-        return array_to_return;
+}
+
+int resizeOrder(int* iref, int* jref, int* shapeOrder, int orderSize, int shapeID, bool skipLine) {
+        int i = *iref;
+        int j = *jref;
+        cout << "OrderSize : " << orderSize << "\n";
+        shapeOrder[orderSize] = shapeID;
+        cout << "ShapeID : " << shapeID << "\n";
+        
+        orderSize++;
+        if(skipLine) {
+                i = 2*j - i + 4;
+        } else {
+                i = j + 2;
+        }
+        *iref = i;
+        *jref = j;
+        return orderSize;
 }
